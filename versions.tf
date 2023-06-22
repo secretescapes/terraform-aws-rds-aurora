eaacc9278791d3c3de35365f3b8e922777ca9ee7
Ian Collar <ian.collar@secretescapes.com>
2023-06-22 13:55:37 +0100

    Update versions

--- versions.tf
+++ versions.tf
@@ -1,8 +1,7 @@
 terraform {
-  required_version = ">= 0.12.6"
+  required_version = ">= 1.4.6"
 
   required_providers {
-    aws    = ">= 2.45"
-    random = ">= 2.2"
+    aws    = "< 5.0.0"
   }
 }
